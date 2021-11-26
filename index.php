e9be73a3da5ead4b9bc27dd2917ce290f17dafb2
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-11-26 15:04:53 +0100

    add variables and with echo print in html

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+// Variabile testo di prova
+$textExemple = "Questo è il mio primo testo di prova utilizzando PHP!";
+
+// Variabile corrispondente alla lunghezza totale del testo di prova
+$lengthText = strlen($textExemple);
+
+// Variabile inserita nell'url dall'utente
+$userWord = $_GET["userText"];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +22,16 @@
 </head>
 
 <body>
+    <h1>PHP Badwords</h1>
 
+    <!-- Testo esempio -->
+    <p><?php echo $textExemple ?></p>
+
+    <!-- Caratteri totali variabile testo esempio -->
+    <p>Totale caratteri: "<?php echo $lengthText ?>"</p>
+
+    <!-- testo inserito dall'utente tramite url e modificato con 3 asterischi -->
+    <p>Testo Utente: <?php echo str_replace($userWord, '***', $userWord) ?></p>
 </body>
 
 </html>
